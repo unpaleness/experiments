3254115f1640f871e8175f7fa888b2b06486b7c6
unpaleness <generalissimusunpaleness@gmail.com>
2019-06-21 12:16:57 +0300

    add Add, Sub and Multi actions for string ints

--- string_int.cpp
+++ string_int.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 std::string Usage() { return "Usage: <executable> fig1 fig2\n"; }
 
@@ -22,6 +23,16 @@ void TrimInt(std::string &fig) {
   }
 }
 
+std::string &&TrimInt(std::string &&fig) {
+  size_t first_non_zero{fig.find_first_not_of('0')};
+  if (first_non_zero == std::string::npos) {
+    fig.clear();
+  } else {
+    fig.erase(0, first_non_zero);
+  }
+  return std::move(fig);
+}
+
 std::string Add(const std::string &fig1, const std::string &fig2) {
   if (fig1.empty()) {
     return fig2;
@@ -29,7 +40,87 @@ std::string Add(const std::string &fig1, const std::string &fig2) {
   if (fig2.empty()) {
     return fig1;
   }
-  std::vector<unsigned short int> result;
+  size_t length{fig1.size() > fig2.size() ? fig1.size() : fig2.size()};
+  int higher_digit{0};
+  std::vector<int> result_vector;
+  for (size_t i = 0; i < length; ++i) {
+    int sum{higher_digit};
+    if (i >= fig1.size()) {
+      sum += fig2[fig2.size() - i - 1] - '0';
+    } else if (i >= fig2.size()) {
+      sum += fig1[fig1.size() - i - 1] - '0';
+    } else {
+      sum += fig1[fig1.size() - i - 1] - '0' + fig2[fig2.size() - i - 1] - '0';
+    }
+    result_vector.push_back(sum % 10);
+    higher_digit = sum / 10;
+  }
+  if (higher_digit > 0) {
+    result_vector.push_back(higher_digit);
+  }
+  std::string result_string;
+  for (auto i = result_vector.rbegin(); i < result_vector.rend(); ++i) {
+    result_string += std::to_string(*i);
+  }
+  return result_string;
+}
+
+std::string Sub(const std::string &fig1, const std::string &fig2) {
+  if (fig1.empty() || fig1 <= fig2) {
+    return "";
+  }
+  if (fig2.empty()) {
+    return fig1;
+  }
+  int higher_digit{0};
+  std::vector<int> result_vector;
+  for (size_t i = 0; i < fig1.size(); ++i) {
+    int sub{higher_digit};
+    if (i >= fig2.size()) {
+      sub += fig1[fig1.size() - i - 1] - '0';
+    } else {
+      sub +=
+          fig1[fig1.size() - i - 1] - '0' - (fig2[fig2.size() - i - 1] - '0');
+    }
+    if (sub < 0) {
+      result_vector.push_back(sub + 10);
+      higher_digit = -1;
+    } else {
+      result_vector.push_back(sub);
+      higher_digit = 0;
+    }
+  }
+  std::string result_string;
+  for (auto i = result_vector.rbegin(); i < result_vector.rend(); ++i) {
+    result_string += std::to_string(*i);
+  }
+  return result_string;
+}
+
+std::string Multi(const std::string &fig1, const std::string &fig2) {
+  if (fig1.empty()) {
+    return "";
+  }
+  if (fig2.empty()) {
+    return "";
+  }
+  std::vector<int> result_vector(fig1.size() + fig2.size(), 0);
+  for (size_t j = 0; j < fig1.size(); ++j) {
+    int higher_digit{0};
+    for (size_t i = 0; i < fig2.size(); ++i) {
+      int temp{result_vector[j + i] + higher_digit +
+               (fig1[fig1.size() - j - 1] - '0') *
+                   (fig2[fig2.size() - i - 1] - '0')};
+      higher_digit = temp / 10;
+      result_vector[j + i] = temp % 10;
+    }
+    result_vector[j + fig2.size()] += higher_digit;
+  }
+  std::string result_string;
+  for (auto i = result_vector.rbegin(); i < result_vector.rend(); ++i) {
+    result_string += std::to_string(*i);
+  }
+  return result_string;
 }
 
 int main(int argc, char **argv) {
@@ -51,5 +142,8 @@ int main(int argc, char **argv) {
   } else {
     std::cout << "They are not equal" << std::endl;
   }
+  std::cout << "Sum is " << Add(fig1, fig2) << std::endl;
+  std::cout << "Sub is " << TrimInt(Sub(fig1, fig2)) << std::endl;
+  std::cout << "Multi is " << TrimInt(Multi(fig1, fig2)) << std::endl;
   return 0;
 }
